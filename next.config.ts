3915748a25fbfa259c6a30a526dec7dcafb61177
MYSELFFABHISHEKK <ajhaaa3083@gmail.com>
2025-06-26 02:09:01 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100MB",
+    },
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "nyc.cloud.appwrite.io",
+      },
+    ],
+  },
+
+  reactStrictMode: true,
+  // Remove experimental.turbopack if unused
 };
 
 export default nextConfig;
